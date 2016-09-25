c711af8d97b63419282d20da82a82e088754e73e
Vritvij Kadam <vritvijkadam@gmail.com>
2016-09-24 18:06:01 -0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	index.html

--- js/main.js
+++ js/main.js
@@ -1,6 +1,3 @@
 $(document).ready(function(){
-    $('.section').scrollSpy({
-        scrollOffset: 0
-    }); //apply to elements to be tracked
-    $('#nav').pushpin(); //apply to element to be pinned
+
 });
